8d621212a8e5b9fdb80ea88766211ba5d7d741f7
rogeriodelphi <rogeriodelphi@gmail.com>
2022-07-17 22:44:21 -0300

    Escutando os eventos - campo required

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -14,14 +14,20 @@ const Formulario = () => {
     'Mobile',
     'Inovação e Gestão'
   ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido')
+  }
+
   return (
     <section className='formulario'>
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-        <ListaSuspensa label="Times" itens={times}/>
+        <ListaSuspensa obrigatorio={true} label="Times" itens={times}/>
         <Botao>
           Criar Card
         </Botao>  
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -6,7 +6,7 @@ import './ListaSuspensa.css'
     return (
       <div className='lista-suspensa'>
         <label>{props.label}</label>
-        <select>
+        <select required={props.required}>
           {props.itens.map(item => {
             return <option key={item}>{item}</option>
           })}
