b8e0d81c29b20baf61dc938d7daca1a2856b4720
rogeriodelphi <rogeriodelphi@gmail.com>
2022-07-17 22:27:11 -0300

    Criando uma lista suspensa no formulario

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,34 @@
+import Botao from '../Botao';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import './Formulario.css'
+
+
+const Formulario = () => {
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'Ux e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+  return (
+    <section className='formulario'>
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+        <ListaSuspensa label="Times" itens={times}/>
+        <Botao>
+          Criar Card
+        </Botao>  
+      </form>
+    </section>
+  )
+
+}
+
+export default Formulario
